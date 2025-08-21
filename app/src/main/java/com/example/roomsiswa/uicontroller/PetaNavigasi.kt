552e513b161f105c4e6a7829d2c8a0791f94e001
Ananda Agung Ismail <158981471+CaptainAAI@users.noreply.github.com>
2025-08-21 12:09:01 +0000

    menampilkan top app bar dan konten utama dengan navigasi antar layar menggunakan NavHostController

--- app/src/main/java/com/example/roomsiswa/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomsiswa/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,30 @@
 package com.example.roomsiswa.uicontroller
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    // Get the current back stack entry to determine the current screen
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = currentBackStackEntry?.destination?.route ?: DestinasiHome.route
+
+    // Determine if the "back" button should be shown
+    val canNavigateBack = navController.previousBackStackEntry != null
+
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(id = findTitleByRoute(currentRoute)),
+                canNavigateBack = canNavigateBack,
+                navigateUp = { navController.navigateUp() }
+            )
+        }
+    ) { innerPadding ->
+        HostNavigasi(
+            navController = navController,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
