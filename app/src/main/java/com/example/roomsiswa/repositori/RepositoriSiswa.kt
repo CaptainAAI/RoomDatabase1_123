821824b8df5b1f0cf2f9d157aed293d6c69013ad
Ananda Agung Ismail <158981471+CaptainAAI@users.noreply.github.com>
2025-08-21 08:17:08 +0000

    4 Mengembalikan aliran (stream) data daftar siswa secara reaktif menggunakan Flow

---
+++ app/src/main/java/com/example/roomsiswa/repositori/RepositoriSiswa.kt
@@ -0,0 +1,12 @@
+package com.example.roomsiswa.repositori
+
+import com.example.roomsiswa.room.Siswa
+import com.example.roomsiswa.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa
+{
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
