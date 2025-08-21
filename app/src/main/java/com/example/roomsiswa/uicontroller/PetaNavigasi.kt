abd2ac4f9e39c87b1aa5697347b18612e103f90a
Ananda Agung Ismail <158981471+CaptainAAI@users.noreply.github.com>
2025-08-21 12:09:44 +0000

    Composable for handling navigation between screens.

--- app/src/main/java/com/example/roomsiswa/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomsiswa/uicontroller/PetaNavigasi.kt
@@ -54,3 +54,26 @@ fun SiswaTopAppBar(
         }
     )
 }
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
